a0b7f287996f1f65b3b67f23e0c23384cdee010a
louissosthenes9 <louissosthenes9@gmail.com>
2024-04-13 12:55:41 +0300

    products table complete

--- backend/database/migrations/2024_03_28_224604_create_products_table.php
+++ backend/database/migrations/2024_03_28_224604_create_products_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -19,12 +20,12 @@ return new class extends Migration
             $table->string("image_mime")->nullable();
             $table->string("image_size")->nullable();
             $table->longText("description")->nullable();
-            $table->decimal('price',10,2);
-            $table->foreignIdFor(User::class, 'created_by')->nullable();
+            $table->decimal('price',10);
+            $table->foreignIdFor( User::class, 'created_by')->nullable();
             $table->foreignIdFor(User::class, 'updated_by')->nullable();
             $table->foreignIdFor(User::class, 'deleted_by')->nullable();
             $table->softDeletes();
-            
+
             $table->timestamps();
         });
     }
