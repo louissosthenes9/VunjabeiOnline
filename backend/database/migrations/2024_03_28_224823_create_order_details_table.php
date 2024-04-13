5ba63dfa2c441999a734ef81c8565c3636df0c5c
louissosthenes9 <louissosthenes9@gmail.com>
2024-04-13 12:57:18 +0300

    order_details table

--- backend/database/migrations/2024_03_28_224823_create_order_details_table.php
+++ backend/database/migrations/2024_03_28_224823_create_order_details_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('order_details', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('larst_name');
+            $table->string('phone')->nullable();
+            $table->string('address1');
+            $table->string('address2');
+            $table->string('city');
+            $table->string('state');
+            $table->string('zipcode');
+            $table->string('country_code');
             $table->timestamps();
         });
     }
