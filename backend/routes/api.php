581163b0b4f7a28b53e3e86ab182785336623b66
louissosthenes9 <louissosthenes9@gmail.com>
2024-06-15 14:18:55 +0300

    added routes and fields

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +15,22 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::middleware(['auth:sanctum'])->group(
+    function () {
+        Route::get('/product/{product:slug}', [ProductController::class, 'index']);
 
+
+        Route::prefix('/cart')->name('cart.')->group(function () {
+              Route::get('/', [CartController::class,'index']);
+              Route::get('/add/{product:slug}', [CartController::class,'add'])->name('add');
+              Route::get('/remove/{product:slug}', [CartController::class,'remove'])->name('remove');
+              Route::get('/updated-quantity/{product:slug}', [CartController::class,'updateQuantity'])->name('update-quantity');
+
+
+        });
+
+        }
+);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
